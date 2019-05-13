937362c4b089c2e8031ebfb04216151e12c2b9ea
Santa Claus <santa@northpole.com>
2019-05-13 08:29:16 +0300

    classmethod inputs

--- password-locker/user.py
+++ password-locker/user.py
@@ -21,4 +21,28 @@ class User:
         '''
         User.user_list.remove(self)
 
-   
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list      
+
+    @classmethod
+    def user_exist(cls,password):
+        '''
+        method to check if a user and
+        their details exist
+        '''
+        for user in cls.user_list:
+            if  user.password == password:
+                return True
+        return False    
+    @classmethod
+    def find_account(cls,password):
+        '''
+        method that finds an account by its name
+        '''
+        for user in cls.user_list:
+            if user.password == password:
+                return user    
