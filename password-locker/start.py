d8afa4c77d88b8396c8b56dc03cef212c184ffad
Santa Claus <santa@northpole.com>
2019-05-13 07:54:29 +0300

    updated user and credentials

--- password-locker/start.py
+++ password-locker/start.py
@@ -18,3 +18,39 @@ def create_credentials(account_name,password):
     new_credentials = User(account_name,password)
     return new_credentials 
 
+def save_user(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+ 
+def save_credentials(self):
+    '''
+    save_user method to save new users into the list
+    '''
+    Credentials.credentials_list.append(self)
+
+def check_existing_user(password):
+    '''
+    function to check that enable login authentification
+    '''
+    return User.user_exist(password)
+
+def find_account(password):
+    '''
+    function to find account by its name
+    '''
+    return User.find_account(password)
+
+def delete_credentials(self):
+    '''
+    delete method to delete saved information from the list
+    '''
+    Credentials.credentials_list.remove(self)
+
+def display_credentials():
+    '''
+    method that returns the user list
+    '''
+    return User.display_users()
+
